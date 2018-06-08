59500037ac15a799483f7964bbddd3ffddf3a35c
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-06-08 11:36:14 -0300

    funcionalidad

--- public/javascripts/funciones.js
+++ public/javascripts/funciones.js
@@ -204,3 +204,17 @@ function getComplejo(complejoId){
 			target = jsonComplejos[i];
 	return target;
 }
+
+function pedirClima() {
+
+  $.get('/clima', function(data){
+        if (data != [])
+            mostrarClima(data);
+      }
+}
+
+
+function mostrarClima(data){
+    swal(text: data.WeatherText);
+  //aca va el swal con los case, o podria simplemente mostrar el mensaje que me da accu.
+}
