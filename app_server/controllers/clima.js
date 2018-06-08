906a3aac04ae547a9c365d50548c5a13660c1e8e
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-06-08 14:44:24 -0300

    requires

--- app_server/controllers/clima.js
+++ app_server/controllers/clima.js
@@ -6,13 +6,18 @@ clima.getClima = function(req,res){
 var url1 = 'http://dataservice.accuweather.com/currentconditions/v1/2931?apikey=';
 var key = 'bGpGXSIQ9WuaTxxGEMzi5GF7FfkMLvsD';
 var config = '&language=es&details=false';
-var url2 = url1+key+config;
-request.get(url2,function(err,res2,body){
-  if(res2.statusCode != 200)
-        res.status(res2.statusCode);
-        res.JSON(res2);
+var url2 = 'http://dataservice.accuweather.com/currentconditions/v1/2931?apikey=bGpGXSIQ9WuaTxxGEMzi5GF7FfkMLvsD&language=es&details=false';
+request({
+  uri: url2,
+  method: "GET",
+  timeout: 10000,
+  followRedirect: true,
+  maxRedirects: 10
+}, function(error, response, body) {
+  console.log(body);
+      res.JSON(body);
 });
-};
 
+}
 
 module.exports = clima;
