373f60e7ff8666023ef2d43788bf904de0a1af77
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-06-08 14:09:05 -0300

    con get

--- app_server/controllers/clima.js
+++ app_server/controllers/clima.js
@@ -6,6 +6,12 @@ clima.getClima = function(req,res){
       var key = 'bGpGXSIQ9WuaTxxGEMzi5GF7FfkMLvsD';
       var config = '&language=es&details=false';
       var url2 = url1+key+config;
+      $.get(url2,function(data){
+              res.status(200);
+              res.JSON(data);
+
+      });
+      /*
       $.ajax({
     	    url: url2,
     	    type: 'get',
@@ -16,6 +22,7 @@ clima.getClima = function(req,res){
                res.JSON(data);
              },
             });
+*/
 
 }
 
