9d73c2fce04ed85dc162568f1cbe28efe40f7bf7
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-06-08 12:41:28 -0300

    probando

--- public/javascripts/comentarios.js
+++ public/javascripts/comentarios.js
@@ -124,3 +124,16 @@ function ocultarComentarios(){
 	//Funcion para ocultar los comentarios de cualquier complejo que se esten mostrando
 	$("#row-comments").fadeOut();
 }
+
+function pedirClima(){
+
+  $.get('/clima', function(data){
+        if (data != [])
+            mostrarClima(data);
+      });
+}
+
+function mostrarClima(data){
+    swal({text: data.WeatherText});
+  //aca va el swal con los case, o podria simplemente mostrar el mensaje que me da accu.
+}
