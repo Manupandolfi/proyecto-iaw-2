df8f63b9f8fc607f8685be1ba4fa775e298aa141
DESKTOP-IBGV4OO\Alan <alan.vzuk@gmail.com>
2018-06-09 19:49:31 -0300

    clima

--- app_server/controllers/clima.js
+++ app_server/controllers/clima.js
@@ -8,14 +8,7 @@ var url1 = 'http://dataservice.accuweather.com/currentconditions/v1/2931?apikey=
 var key = 'bGpGXSIQ9WuaTxxGEMzi5GF7FfkMLvsD';
 var config = '&language=es&details=false';
 var url2 = 'http://dataservice.accuweather.com/currentconditions/v1/2931?apikey=bGpGXSIQ9WuaTxxGEMzi5GF7FfkMLvsD&language=es&details=false';
-request({
-  uri: url2,
-  method: "GET",
-  timeout: 10000,
-  followRedirect: true,
-  datatype: "JSON",
-  maxRedirects: 10
-}, function(error, response, body) {
+request(url2, function(error, response, body) {
       var miJson = JSON.parse(body);
       console.log(miJson.WeatherText);
       console.log(body.WeatherText);
